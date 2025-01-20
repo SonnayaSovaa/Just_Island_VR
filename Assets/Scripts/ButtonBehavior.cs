ca38300c349564dfdc4b03548f8d55f8aaad2caf
Ксения Нагишева <knagisheva@gmail.com>
2025-01-20 23:20:07 +0300

    UI sound effects

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -4,11 +4,14 @@ public class ButtonBehavior : MonoBehaviour
 {
     private Vector3 _startScale;
     [SerializeField] private float scaleFactor;
+    [SerializeField] private AudioClip onHover;
+    [SerializeField] private AudioSource source;
 
     public void OnHover(Transform obj)
     {
         _startScale = obj.localScale;
         obj.localScale *= scaleFactor;
+        source.PlayOneShot(onHover);
     }
 
     public void ExitHover(Transform obj)
