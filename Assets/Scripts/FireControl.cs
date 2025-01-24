1f11449b90a2a4fd37bb5bb8087fc287e60e2866
Ксения Нагишева <knagisheva@gmail.com>
2025-01-24 22:24:45 +0300

    Island +

--- Assets/Scripts/FireControl.cs
+++ Assets/Scripts/FireControl.cs
@@ -8,6 +8,10 @@ public class FireControl : MonoBehaviour
     [SerializeField] private Light fire;
     [SerializeField] private float freq;
 
+    [SerializeField] private float min;
+    [SerializeField] private float max;
+
+
     private void OnEnable()
     {
         StartCoroutine(Fire());
@@ -17,7 +21,7 @@ public class FireControl : MonoBehaviour
     {
         while (freq != 0)
         {
-            fire.intensity = Random.Range(10, 30);
+            fire.intensity = Random.Range(min, max);
             yield return new WaitForSeconds(freq);
         }
 
